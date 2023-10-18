e723275e16aaed5f18229bb40c3cded349b04e25
PCBMoreira2 <pcbmoreira@id.uff.br>
2023-10-18 12:04:57 -0300

    Feat|Added search funcionality

--- agenda/busca_contatos.py
+++ agenda/busca_contatos.py
@@ -1,1 +1,4 @@
-python
+def buscar_contato(nome, lista):
+    for contato in lista:
+        if contato == nome:
+            return contato
--- agenda/buscar.py
+++
@@ -1,4 +0,0 @@
-def buscar_contato(nome, lista):
-    for contato in lista:
-        if contato == nome:
-            return contato
